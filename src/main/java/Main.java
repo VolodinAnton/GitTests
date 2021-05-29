aee64812ed33fda092015c4db1f0844c419cc47d
VolodinAnton <73581346+VolodinAnton@users.noreply.github.com>
2021-05-29 23:12:36 +1000

    Add Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Man father = new Man("Anton", 35);
+        Woman mother = new Woman("Irina", 37);
+
+        System.out.println(father);
+        System.out.println(mother);
+    }
+}
